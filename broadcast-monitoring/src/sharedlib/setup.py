c5d6769b8e7d4504b86bfadf718189c456ccbd3c
Lionel Gattegno <gattegno@amazon.fr>
2022-05-06 13:18:27 +0200

    remove description as it prevents the solution to build successfully

--- broadcast-monitoring/src/sharedlib/setup.py
+++ broadcast-monitoring/src/sharedlib/setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Broadcast Monitoring Lambda Helpers",
+    name="Broadcast-Monitoring-Lambda-Helpers",
     version="0.0.1",
     description="Helper classes for developing Broadcast Monitoring Lambda Functions",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     install_requires=[
         'boto3',
